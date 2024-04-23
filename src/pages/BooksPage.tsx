ba9c93cd12af9bb4690275fcedeb55697ea7fbe9
Rakesh K <codersgyan@gmail.com>
2024-04-23 13:25:12 +0300

    add dashboard layout

---
+++ src/pages/BooksPage.tsx
@@ -0,0 +1,5 @@
+const BooksPage = () => {
+    return <div>BooksPage</div>;
+};
+
+export default BooksPage;
--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,23 @@ import { createBrowserRouter } from 'react-router-dom';
 import LoginPage from '@/pages/LoginPage';
 import HomePage from '@/pages/HomePage';
 import RegisterPage from './pages/RegisterPage';
+import DashboardLayout from './layouts/DashboardLayout';
+import BooksPage from './pages/BooksPage';
 
 const router = createBrowserRouter([
     {
-        path: '/',
-        element: <HomePage />,
+        path: 'dashboard',
+        element: <DashboardLayout />,
+        children: [
+            {
+                path: 'home',
+                element: <HomePage />,
+            },
+            {
+                path: 'books',
+                element: <BooksPage />,
+            },
+        ],
     },
     {
         path: '/login',
