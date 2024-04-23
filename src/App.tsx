1439f4675a5f80ca68c0dfa0e7d25419bfb9e3df
Rakesh K <codersgyan@gmail.com>
2024-04-23 12:01:04 +0300

    test shadcn ui button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
     return (
         <>
             <h1>Welcome to elib dashboard</h1>
+            <Button>Click me</Button>
         </>
     );
 }
