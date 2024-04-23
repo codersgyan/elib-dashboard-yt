9dfb4aedd17a0249fd24a949072e88bdbeafffbf
Rakesh K <codersgyan@gmail.com>
2024-04-23 14:32:26 +0300

    display errors

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -47,8 +47,10 @@ const LoginPage = () => {
                 <CardHeader>
                     <CardTitle className="text-2xl">Login</CardTitle>
                     <CardDescription>
-                        Enter your email below to login to your account.
-                        {mutation.isPending && <div>Loading...</div>}
+                        Enter your email below to login to your account. <br />
+                        {mutation.isError && (
+                            <span className="text-red-500 text-sm">{'Something went wrong'}</span>
+                        )}
                     </CardDescription>
                 </CardHeader>
                 <CardContent className="grid gap-4">
