a6cf6c80fa90f4b1e466a1a49bd3093b2a96188d
Rakesh K <codersgyan@gmail.com>
2024-04-23 13:27:40 +0300

    add auth layout

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import HomePage from '@/pages/HomePage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardLayout from './layouts/DashboardLayout';
 import BooksPage from './pages/BooksPage';
+import AuthLayout from './layouts/AuthLayout';
 
 const router = createBrowserRouter([
     {
@@ -21,12 +22,18 @@ const router = createBrowserRouter([
         ],
     },
     {
-        path: '/login',
-        element: <LoginPage />,
-    },
-    {
-        path: '/register',
-        element: <RegisterPage />,
+        path: '/auth',
+        element: <AuthLayout />,
+        children: [
+            {
+                path: 'login',
+                element: <LoginPage />,
+            },
+            {
+                path: 'register',
+                element: <RegisterPage />,
+            },
+        ],
     },
 ]);
 
