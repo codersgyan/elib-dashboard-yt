512a5f1d12b1f4b67f25b3ef5cb97a84e276c66a
Rakesh K <codersgyan@gmail.com>
2024-04-23 12:26:12 +0300

    add login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,41 @@
+import { Button } from '@/components/ui/button';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
 const LoginPage = () => {
-    return <div>Login</div>;
+    return (
+        <section className="flex justify-center items-center h-screen">
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="grid gap-4">
+                    <div className="grid gap-2">
+                        <Label htmlFor="email">Email</Label>
+                        <Input id="email" type="email" placeholder="m@example.com" required />
+                    </div>
+                    <div className="grid gap-2">
+                        <Label htmlFor="password">Password</Label>
+                        <Input id="password" type="password" required />
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    <Button className="w-full">Sign in</Button>
+                </CardFooter>
+            </Card>
+        </section>
+    );
 };
 
 export default LoginPage;
