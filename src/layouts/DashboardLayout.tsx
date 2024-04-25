6740bd2d7a706d8e3329db7959e9137ed9835b08
Rakesh K <codersgyan@gmail.com>
2024-04-25 14:24:49 +0300

    add active link for nav menu

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -24,7 +24,7 @@ import {
     ShoppingCart,
     Users,
 } from 'lucide-react';
-import { Link, Navigate, Outlet } from 'react-router-dom';
+import { Link, Navigate, NavLink, Outlet } from 'react-router-dom';
 
 const DashboardLayout = () => {
     const { token, setToken } = useTokenStore((state) => state);
@@ -54,19 +54,27 @@ const DashboardLayout = () => {
                     </div>
                     <div className="flex-1">
                         <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-                            <Link
+                            <NavLink
                                 to="/dashboard/home"
-                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+                                className={({ isActive }) => {
+                                    return `flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${
+                                        isActive && 'bg-muted'
+                                    }`;
+                                }}>
                                 <Home className="h-4 w-4" />
                                 Home
-                            </Link>
+                            </NavLink>
 
-                            <Link
+                            <NavLink
                                 to="/dashboard/books"
-                                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary">
+                                className={({ isActive }) => {
+                                    return `flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${
+                                        isActive && 'bg-muted'
+                                    }`;
+                                }}>
                                 <Package className="h-4 w-4" />
                                 Books{' '}
-                            </Link>
+                            </NavLink>
                         </nav>
                     </div>
                     <div className="mt-auto p-4">
